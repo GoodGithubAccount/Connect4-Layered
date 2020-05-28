76d54b250279d97c769a641989580b1eeb0b5c65
KaNN <lasseg123@hotmail.com>
2020-05-28 22:06:06 +0200

    Objects and boards semi redone

--- src/Server/Game/Board.java
+++ src/Server/Game/Board.java
@@ -7,10 +7,14 @@ public class Board {
     private int layers;
     private int layerLevel;
 
-    private BoardMap[][] boardState;
+    private BoardMap myBoard;
 
     private int currentLevel;
-    private BoardMap[][] currentObject;
+    private BoardMap currentObject;
+    private BoardMap[][] currentBoard;
+
+    // needs to be rewritten so we have currentObject, and currentBoard.
+    // object needs to get the actual object, board needs to keept track of the current board, getting it from the current object.
 
     public Board(int height, int width, int layers){
         this.height = height;
@@ -18,18 +22,11 @@ public class Board {
         this.layers = layers;
         layerLevel = 0;
 
-        boardState = new BoardMap[height][width];
-        currentObject = boardState;
+        myBoard = new BoardMap(this.height, this.width, this.layers, layerLevel, null);
+        currentBoard = myBoard.getBoardMap();
+        currentObject = myBoard;
+        currentObject.generateLayer();
         currentLevel = 0;
-        generateBoard();
-    }
-
-    private void generateBoard(){
-        for(int i = 0; i < height; i++){
-            for(int a = 0; a < width; a++){
-                boardState[i][a] = new BoardMap(height, width, layers, layerLevel, boardState);
-            }
-        }
     }
 
     private boolean hwCheck(int y, int x){
@@ -50,16 +47,25 @@ public class Board {
         for(int i = 0; i < height; i++){
             System.out.println();
             for(int a = 0; a < height; a++){
-                System.out.print(currentObject[i][a].getLayerLevel() + "-" + i+ "-" + a + "  ");
+                System.out.print(currentObject.getLayerLevel() + "-" + i+ "-" + a + "  ");
             }
+        }
+    }
+
+    public void goBack(){
+        currentObject = currentObject.getParent();
+        if(currentObject == null){
+            currentObject = myBoard;
         }
     }
 
     public void traverseBoard(int y, int x){
         // Remember to make y zero later.
         if(hwCheck(y, x) && currentLevel < layers){
-            currentObject[y][x].generateLayer();
-            currentObject = currentObject[y][x].getBoardMap();
+            currentObject = currentBoard[y][x];
+            currentObject.generateLayer();
+            currentBoard = currentObject.getBoardMap();
+
             currentLevel++;
         }
     }
--- src/Server/Game/BoardMap.java
+++ src/Server/Game/BoardMap.java
@@ -8,24 +8,32 @@ public class BoardMap {
     private int layerLevel;
 
     private BoardMap[][] boardMap;
-    private BoardMap[][] parentBoard;
+    private BoardMap parent;
 
-    public BoardMap(int height, int width, int layers, int layerLevel, BoardMap[][] parentBoard){
+    private int[][] owner;
+
+    public BoardMap(int height, int width, int layers, int layerLevel, BoardMap parent){
         this.layers = layers - 1;
         this.layerLevel = layerLevel + 1;
         this.height = height;
         this.width = width;
-        this.parentBoard = parentBoard;
+        this.parent = parent;
 
+        owner = new int[height][width];
         boardMap = new BoardMap[height][width];
     }
 
     public void generateLayer(){
         for(int i = 0; i < height; i++){
             for(int a = 0; a < width; a++){
-                boardMap[i][a] = new BoardMap(height, width, layers, layerLevel, boardMap);
+                owner[i][a] = 0;
+                boardMap[i][a] = new BoardMap(height, width, layers, layerLevel, this);
             }
         }
+    }
+
+    public BoardMap getParent(){
+        return parent;
     }
 
     public BoardMap[][] getBoardMap(){
